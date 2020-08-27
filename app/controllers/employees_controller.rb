3c880cf9b54c9bd54ecb7ca1202b8dc7edb5b4b9
anickel101 <anson.nickel@gmail.com>
2020-08-27 17:46:45 -0400

    finished pair programming

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,48 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        a_check = @employee.alias_valid?(@employee.alias)
+        t_check = @employee.title_valid?(@employee.title)
+        if a_check && t_check
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            redirect_to employees_path
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        a_check = @employee.alias_valid?(@employee.alias)
+        t_check = @employee.title_valid?(@employee.title)
+        if a_check && t_check
+            @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else
+            redirect_to employees_path
+        end
+    end
+
+    private
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,16 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
+
+    def alias_valid?(a)
+        !Employee.all.find {|employee| employee.alias == a}
+    end
+
+    def title_valid?(t)
+        !Employee.all.find {|employee| employee.title == t}
+    end
+
 end
