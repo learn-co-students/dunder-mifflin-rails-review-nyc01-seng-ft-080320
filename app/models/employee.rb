0ff781e508963627ce8ed53b4ba8385d5f4e145b
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-27 17:50:00 -0400

    added the validations for the employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,17 @@
 class Employee < ApplicationRecord
     belongs_to :dog
-
+    validate :is_valid?
     def full_name
         "#{self.first_name} #{self.last_name}"
+    end
+
+    private
+    def is_valid?
+        if Employee.find_by(alias: self.alias)
+            errors.add(:alias, "Cannot be the same as someone else's")
+        end
+        if Employee.find_by(title: self.title)
+            errors.add(:title, "Cannot be the same as someone else's")
+        end
     end
 end
