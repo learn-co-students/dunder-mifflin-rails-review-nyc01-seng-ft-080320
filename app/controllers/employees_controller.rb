72091efebf41ae011febf4cf9f7a8053ff6eda55
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-27 17:42:44 -0400

    Committing the forms for employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -14,11 +14,13 @@ class EmployeesController < ApplicationController
     end
 
     def create
-        Employee.create(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+        employee = Employee.create(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+        redirect_to employee_path(employee)
     end
 
     def update
         @employee.update(params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+        redirect_to employee_path(@employee)
     end
 
     def edit
