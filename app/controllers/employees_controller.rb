bc9249cc9bd3bef6a19a67e1ba2da88462995987
alerner1 <alyssplerner@gmail.com>
2020-08-27 16:25:39 -0400

    first two deliverables

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,45 @@
 class EmployeesController < ApplicationController
+  before_action :find_employee, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @employee = Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+
+    redirect_to employee_path(@employee)
+  end
+
+  def edit
+  end
+
+  def update
+    @employee.update(employee_params)
+
+    redirect_to employee_path(@employee)
+  end
+
+  def destroy
+    @employee.destroy
+
+    redirect_to employees_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog)
+  end
+
+  def find_dog
+    @employee = Employee.find(params[:id])
+  end
 end
