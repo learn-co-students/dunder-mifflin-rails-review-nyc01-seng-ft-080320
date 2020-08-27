48a914713a4e70c3e96373cf5d89ff046b8e7e9f
AmSmo <adam.smolenski@gmail.com>
2020-08-27 17:39:41 -0400

    validations included

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+    has_many :owners, foreign_key: "dog_id", class_name: "Employee"
+
+    def dog_info
+        "Name: " + self.name + "| Breed: " + self.breed + "| Age: " + self.age
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    validates :title, uniqueness: true
+    validates :alias, uniqueness: true
+    belongs_to :dog
+
+
+    def name
+        "#{first_name} #{last_name}"
+    end
 end
