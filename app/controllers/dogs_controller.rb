fdb655e6803ce46b6868773533bc4624225c8b91
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-27 18:40:36 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,13 @@
 class DogsController < ApplicationController
     def index
         @dogs = Dog.all
+        if params.include? :direction
+            if params[:direction] == "asc"
+                @dogs = @dogs.sort_by{|dog| dog.employees.count}
+            elsif params[:direction] == "desc"
+                @dogs = @dogs.sort_by{|dog| dog.employees.count}.reverse
+            end
+        end
     end
 
     def show
