1ec20a844daf5b19c11db1330842ff3d2377927a
kevinpark07 <kevin.park07@yahoo.com>
2020-08-27 17:27:51 -0400

    complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+    before_action :find_employee, only: [:show, :edit, :update, :create]
+
+    def index
+        @employees = Employee.all
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @employee.update(employee_params)
+        # if @employee.update(employee_params)
+        # else
+        #     puts "The title or alias has already been selected. Please try again."
+        #     redirect_to edit_employee_path(@employee)
+        # end
+        # Employee.all.each do |employee|
+        #     if employee.title != @employee.title
+        #         @employee.title
+        #     elsif employee.alias != @employee.alias
+        #         @employee.alias
+        #     else 
+        #     puts "The title or alias has already been selected. Please try again."   
+        #     end
+        # end 
+       redirect_to edit_employee_path(@employee)
+    end
+
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_ur)
+    end
+        
+
+    def find_employee
+        @employee = Employee.find(params[:id])
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates_uniqueness_of :title, :alias
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
+
 end
